24070c170ec50ee8c08d9bee3dbdfc9a8d99dd44
ghostflyby <ghostflyby+git@outlook.com>
2025-12-19 20:56:48 +0800

    refactor: sorted formatter to use subproject configs first

---
+++ gradle-plugin/src/main/kotlin/FormatterEntry.kt
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 ghostflyby
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ * Part of SpotlessDaemon
+ */
+
+package dev.ghostflyby.spotless.daemon
+
+import com.diffplug.spotless.Formatter
+import org.gradle.api.file.FileCollection
+import java.io.File
+
+internal data class FormatterEntry(
+    val files: FileCollection,
+    val formatter: Formatter,
+    val projectPath: String,
+    val projectDir: File,
+)
